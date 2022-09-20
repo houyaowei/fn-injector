9915bf3a5350d3721f13a66f6c30f722c4a4817e
houyuewei <houyaowei@163.com>
2022-09-20 18:12:42 +0800

    :sparkles: 提交1.0版

---
+++ src/index.js
@@ -0,0 +1,36 @@
+/**
+ * 依赖模块注入器
+ * @type {{}}
+ * eg:
+ * Injector.resolve(['service', 'router'], function(service, router, other) {
+ *
+ * })
+ */
+
+// let Injector = window.Injector = {}
+let deps = new Map();
+
+const Injector = {
+  dependences: deps,
+  register:function (k,v) {
+    this.dependences.set(k,v)
+  },
+  resolve: function (_deps, _func,_scope) {
+    let args = [];
+    for(let i = 0, iLen = _deps.length; i < iLen; i++) {
+      //依赖项
+      const dependKey = _deps[i]
+      const dependValue = this.dependences.get(dependKey)
+      if( dependValue ) {
+        args.push( dependValue );
+      } else {
+        throw new Error('Can\'t resolve ' + dependKey);
+      }
+    }
+    //处理参数，将参数拼接在依赖后面
+    return function() {
+      _func.apply(_scope || {}, args.concat(Array.prototype.slice.call(arguments, 0)));
+    }()
+  }
+}
+export default Injector;
